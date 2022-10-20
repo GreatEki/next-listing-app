3a34feafc95aaf9af1330b6c4201aa740a2a2755
GreatEki <ekeneidiagbor@gmail.com>
2022-10-20 10:07:03 +0100

    feat: redirecting within app

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,15 @@
-import React from "react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 
 const NotFound = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      // router.go(-1) // This will redirect user to last url in browser history
+      router.push("/");
+    }, 3000);
+  }, []);
   return (
     <div className="not-found">
       <h1> Oops.... </h1>
