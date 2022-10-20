4952445b18834431a969625e3531d0f3f53246b2
GreatEki <ekeneidiagbor@gmail.com>
2022-10-20 08:57:58 +0100

    chore: initial setup

---
+++ pages/ninjas/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Ninjas = () => {
+  return (
+    <div>
+      <h1> Ninjas Index Page </h1>
+      <p>
+        {" "}
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry&apos;s standard dummy text
+        ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.{" "}
+      </p>
+    </div>
+  );
+};
+
+export default Ninjas;
