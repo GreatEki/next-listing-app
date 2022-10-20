442fe79e5fbd98221cfdb185e0e72ad2bb3ed8d4
GreatEki <ekeneidiagbor@gmail.com>
2022-10-20 11:43:43 +0100

    feat: rendering individual user data to page

--- pages/ninjas/ninja-details/[NinjaDetails].js
+++ pages/ninjas/ninja-details/[NinjaDetails].js
@@ -1,9 +1,36 @@
-import React from "react";
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
 
-const NinjaDetails = () => {
+  const paths = data.map((ninja) => {
+    return {
+      params: { NinjaDetails: ninja.id.toString() },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.NinjaDetails;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data = await res.json();
+
+  return {
+    props: { ninja: data },
+  };
+};
+
+const NinjaDetails = (props) => {
   return (
     <div>
-      <h1> Details Page </h1>
+      <h1> {props.ninja.name} </h1>
+
+      <p>{props.ninja.email}</p>
+      <p>{props.ninja.website}</p>
+      <p>{props.ninja.address?.city}</p>
     </div>
   );
 };
